fd5fc9a4574b8a05c5073efe2ccaf85a5660ec7a
luke01455 <luke0145555@hotmail.co.uk>
2019-10-16 22:04:02 +0100

    added some libraries and more components for user login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import HomePage from './pages/homepage/homepage.component';
 import Header from './components/header/header-component';
 
-function App() {
+import { selectCurrentUser } from './redux/user/user.selectors';
+import { checkUserSession } from './redux/user/user.actions';
+
+const App = ({ checkUserSession, currentUser }) => {
+
+  // useEffect(() => {
+  //   checkUserSession(); 
+  // }, [checkUserSession]);
+
   return (
     <div className="App">
       <Header />
@@ -12,5 +22,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
