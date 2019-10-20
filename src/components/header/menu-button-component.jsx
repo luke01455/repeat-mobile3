b200454ae9abe81b86e914035a44e71c051c2a80
luke01455 <luke0145555@hotmail.co.uk>
2019-10-20 22:13:33 +0100

    added custom hamburger

--- src/components/header/menu-button-component.jsx
+++ src/components/header/menu-button-component.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-import './menu-button.styles.scss';
+import './menu-button.styles.css';
 
 const MenuButton = () => (
-<div className="MenuButtonContainer"> Btn </div>
+<div className="MenuButtonContainer">
+<button class="hamburger hamburger--3dy is-active" type="button">
+  <span class="hamburger-box">
+    <span class="hamburger-inner"></span>
+  </span>
+</button>
+</div>
 )
 
 export default MenuButton;
