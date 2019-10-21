91bcc786b23cd69b74500e74e8499d175711aff7
luke01455 <luke0145555@hotmail.co.uk>
2019-10-21 13:54:34 +0100

    took state from redux and made hamburger animated based off hidden state

--- src/components/header/menu-button-component.jsx
+++ src/components/header/menu-button-component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+
 import { toggleSideMenuHidden } from '../../redux/side-menu/side-menu.actions';
+import { selectSideMenuHidden } from '../../redux/side-menu/side-menu.selectors';
 
 import './menu-button.styles.css';
 
-const MenuButton = ({ toggleSideMenuHidden }) => (
-<div onClick={toggleSideMenuHidden}className="MenuButtonContainer">
-  <button className="hamburger hamburger--3dy is-active" type="button">
+const MenuButton = ({ toggleSideMenuHidden, hidden }) => (
+<div onClick={toggleSideMenuHidden} className="MenuButtonContainer">
+  <button className={`hamburger hamburger--3dy ${hidden ? 'is-active' : ''}`} type="button">
     <span className="hamburger-box">
       <span className="hamburger-inner"></span>
     </span>
@@ -15,8 +17,11 @@ const MenuButton = ({ toggleSideMenuHidden }) => (
 </div>
 )
 
+const mapStateToProps = createStructuredSelector({
+  hidden: selectSideMenuHidden
+})
 const mapDispatchToProps = dispatch => ({
   toggleSideMenuHidden: () => dispatch(toggleSideMenuHidden())
 });
 
-export default connect(null, mapDispatchToProps)(MenuButton);
+export default connect(mapStateToProps, mapDispatchToProps)(MenuButton);
---
+++ src/redux/side-menu/side-menu.selectors.js
@@ -0,0 +1,8 @@
+import { createSelector } from 'reselect';
+
+const selectSideMenu = state => state.sideMenu;
+
+export const selectSideMenuHidden = createSelector(
+    [selectSideMenu],
+    user => user.hidden
+)
