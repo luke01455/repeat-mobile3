7456b1f767b727f9b301d50298f465744b690ec9
luke01455 <luke0145555@hotmail.co.uk>
2019-10-24 16:29:59 +0100

    added text and some basic styling to premium page

--- src/pages/premium/premium-component.jsx
+++ src/pages/premium/premium-component.jsx
@@ -4,14 +4,14 @@ import './premium.styles.scss';
 
 const PremiumPage = () => (
 <div className="premium-container"> 
-    <div> Title </div>
-    <div> Description </div>
-    <list>
-        .item1
-        .item2
-        .item3
-        .item4
-    </list>
+    <div className='PremiumTitle'> Premium </div>
+    <div className='PremiumDescription'> The benefits to becoming a Premium member: </div>
+    <ul className='ListContainer'>
+        <li> HD and Power Saver recording </li>
+        <li> Optional Cloud Storage</li>
+        <li> Location based recording option</li>
+        <li> Help support the team! </li>
+    </ul>
     <div> Purchase </div>
 </div>
 )
