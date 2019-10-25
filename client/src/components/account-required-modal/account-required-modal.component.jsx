921019b1b37097b773718c2c414be2b1838e1801
luke01455 <luke0145555@hotmail.co.uk>
2019-10-25 20:55:48 +0100

    made modal responsive and added some more styling so its useable for now

--- client/src/components/account-required-modal/account-required-modal.component.jsx
+++ client/src/components/account-required-modal/account-required-modal.component.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './account-required-modal.styles.scss';
 
 const AccountReqModal = ({ modalType, hidden }) => (
 <div className={`styled-modal `}>
     <div className='modal-container'>
-
+    <div className='modal-warning'> { modalType === 'Account' ? 
+    'You need an account to access this page' : 
+    'You need to have a premium account to use this feature'}
+    </div>
+    <Link className='modal-button'> Log in </Link>
+    <Link className='modal-button'> sign up </Link >
+    <Link className='modal-button'> close </Link >
     </div>
 </div>
 
