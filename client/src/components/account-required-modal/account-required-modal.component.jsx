553b7e711ecf701883e59245e9cdf583ec76aa2d
luke01455 <luke0145555@hotmail.co.uk>
2019-10-26 20:33:11 +0100

    added options to modal, they will display based on which type of modal appears, also adjusted some styling and html

--- client/src/components/account-required-modal/account-required-modal.component.jsx
+++ client/src/components/account-required-modal/account-required-modal.component.jsx
@@ -1,28 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { selectAccountModalType } from '../../redux/account-modal/account-modal.selectors';
+import { toggleModalOff } from '../../redux/account-modal/account-modal.actions';
 
 import './account-required-modal.styles.scss';
 
-const AccountReqModal = ({ modalType }) => (
+const AccountReqModal = ({ modalType, toggleModalOff}) => (
 <div className={`styled-modal ${!modalType ? 'closed' : ''}`}>
     <div className='modal-container'>
     <div className='modal-warning'> { modalType === 'account' ? 
     'You need an account to access this page' : 
     'You need to have a premium account to use this feature'}
     </div>
-    {/* <Link className='modal-button'> Log in </Link>
-    <Link className='modal-button'> sign up </Link >
-    <Link className='modal-button'> close </Link > */}
+
+    {    
+    modalType === 'account' ? (
+    <div className='modal-button-container'>
+    <Link className='modal-button' to='/signin'> Log in </Link>
+    <Link className='modal-button' to='/signup'> sign up </Link>
     </div>
-</div>
+    ) 
+    :
+    (
+    <Link className='modal-button' to='/premium'> Get premium </Link>
+    )
+  
+  }
+    <div className='modal-button' to='/' onClick={toggleModalOff}> close </div>
+    </div>
+    </div>
 )
 
 const mapStateToProps = createStructuredSelector({
     modalType: selectAccountModalType
   });
 
-export default connect(mapStateToProps)(AccountReqModal);
+  const mapDispatchToProps = dispatch => ({
+  toggleModalOff: () => dispatch(toggleModalOff())
+ });
+
+export default connect(mapStateToProps, mapDispatchToProps)(AccountReqModal);
