c94030335375ea8c0e15aea5f0616254b5c8f1a7
luke01455 <luke0145555@hotmail.co.uk>
2019-10-27 19:00:08 +0000

    made menu links close menu, also made function to do a double onClick function

--- client/src/components/menu/menu.component.jsx
+++ client/src/components/menu/menu.component.jsx
@@ -3,27 +3,34 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { Link } from 'react-router-dom';
 
+import { toggleSideMenuHidden } from '../../redux/side-menu/side-menu.actions';
 import { selectSideMenuHidden } from '../../redux/side-menu/side-menu.selectors';
 import { toggleModalAccount } from '../../redux/account-modal/account-modal.actions';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import './menu.styles.scss';
 
-const Menu = ({ hidden, toggleModalAccount, currentUser }) => {
+const Menu = ({ hidden, toggleModalAccount, currentUser, toggleSideMenuHidden }) => {
+
+  const startModalToggleMenu = () => {
+    toggleModalAccount();
+    toggleSideMenuHidden();
+  }
+
   return (
     <div className={`styled-menu ${hidden ? 'closed' : ''}`}>
       <div className='a-container'>
       { currentUser ?
-        <Link className="menu-link" to="/premium">ACCOUNT</Link> :
-        <div onClick={toggleModalAccount} className="menu-link"> ACCOUNT </div>
+        <Link className="menu-link" onClick={toggleSideMenuHidden} to="/premium">ACCOUNT</Link> :
+        <div onClick={startModalToggleMenu} className="menu-link"> ACCOUNT </div>
         }
-        <Link className="menu-link" to="/settings">
+        <Link className="menu-link" onClick={toggleSideMenuHidden} to="/settings">
           RECORDING SETTINGS
         </Link>
-        <Link className="menu-link" to="/premium">
+        <Link className="menu-link" onClick={toggleSideMenuHidden} to="/premium">
           GO PREMIUM
         </Link>
-        <Link className="menu-link" to="/">
+        <Link className="menu-link" onClick={toggleSideMenuHidden} to="/">
           CONTACT
         </Link>
       </div>
@@ -37,7 +44,8 @@ const mapStateToProps = createStructuredSelector({
 });
 
 const mapDispatchToProps = dispatch => ({
-  toggleModalAccount: () => dispatch(toggleModalAccount())
+  toggleModalAccount: () => dispatch(toggleModalAccount()),
+  toggleSideMenuHidden: () => dispatch(toggleSideMenuHidden())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Menu);
