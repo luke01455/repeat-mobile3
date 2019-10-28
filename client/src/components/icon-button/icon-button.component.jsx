b6f2c680bf11504003034bc2c101a709c14bf6f5
luke01455 <luke0145555@hotmail.co.uk>
2019-10-28 15:40:56 +0000

    completed most of the styling for icon buttons, needs replicating, onclick styling, and media queries

--- client/src/components/icon-button/icon-button.component.jsx
+++ client/src/components/icon-button/icon-button.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import premiumicon from '../../images/premium_icon.png';
 import './icon-button.styles.scss';
 
 const IconButton = ({children, ...props}) => (
     <div className='icon-button-container' {...props}>
-        <div className='image-container'> </div>
+        <div className='image-container'> 
+        <img src={premiumicon} className='icon-image'/>
+        </div>
         <div className='text-container'> {children} </div>
     </div>
 )
