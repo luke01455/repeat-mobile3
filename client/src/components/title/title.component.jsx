cab3462b49b92fc0ef4fa024fb7b02799e244e7f
luke01455 <luke0145555@hotmail.co.uk>
2019-10-28 17:38:44 +0000

    added bare basic logo and background

--- client/src/components/title/title.component.jsx
+++ client/src/components/title/title.component.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import appBackground from '../../images/appbground.png'
+import { Link } from 'react-router-dom';
+
 import './title.styles.scss';
 
 const Title = () => (
+<div className='title-container'>
 <div className='background-container'>
-    <img src={appBackground} className='background' alt='background'>
-    
-        </img>
-        <Link className="title-container" to="/"> Repeat20 </Link>
+    <div className='background'></div>
+</div>
+<Link className="logo-container" to="/">        
+        <div className="logo">
+
+        </div>
+         </Link>
 </div>
 )
 
