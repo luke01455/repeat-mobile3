3f0d38606361f35f5df242b93161f944c02325da
luke01455 <luke0145555@hotmail.co.uk>
2019-10-29 20:47:41 +0000

    added a logo for title img

--- client/src/components/title/title.component.jsx
+++ client/src/components/title/title.component.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import logoImg from '../../images/logoimg.png';
+
 import './title.styles.scss';
 
 const Title = () => (
-<div className='title-container'>
-<div className='background-container'>
-    <div className='background'></div>
-</div>
-<Link className="logo-container" to="/">        
-        <div className="logo">
-        <div className="title"> Repeat20 </div>
+    <div className='title-container'>
+        <div className='background-container'>
+            <div className='background'></div>
         </div>
-         </Link>
-</div>
+        <Link className="logo-container" to="/">
+            <div className="logo">
+                <img className='logo-img' src={logoImg}></img>
+                <div className="title"> Repeat20 </div>
+            </div>
+        </Link>
+    </div>
 )
 
 export default Title;
