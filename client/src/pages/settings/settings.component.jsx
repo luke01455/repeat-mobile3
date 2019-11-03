02a091103aac5e8be0434a4cb894663a3ae1a38e
luke01455 <luke0145555@hotmail.co.uk>
2019-11-03 16:43:01 +0000

    started styling settings page and imported toggle switches

--- client/src/pages/settings/settings.component.jsx
+++ client/src/pages/settings/settings.component.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 
+import ToggleSwitch from '../../components/toggle-switch/toggle-switch.component';
+
 import './settings.styles.scss';
 
 const SettingsPage = () => (
-<div className="settings-container"> 
-    <div className='settings-title'> Settings </div>
-    <ul className='ListContainer'>
-    <li> On / Off </li>
-        <li> HD / LQ / Power Saver  </li>
-        <li> Cloud Storage </li>
-        <li> Select Location</li>     
-    </ul>
-</div>
+    <div className="settings-container">
+        <div className='settings-title'> Settings </div>
+        <div className='list-container'>
+            <span className='list-item'>
+                 On / Off 
+                 <ToggleSwitch />
+                 </span>
+            <span className='list-item'>
+                 HD / LQ / Power Saver 
+                 <ToggleSwitch />
+                  </span>
+            <span className='list-item'> 
+            Cloud Storage 
+            <ToggleSwitch />
+            </span>
+            <span className='list-item'>
+                 Select Location
+                 <ToggleSwitch />
+                 </span>
+        </div>
+    </div>
 )
 
 export default SettingsPage;
