a9597ac6f974e3407715f6b2001dc868efd7111d
luke01455 <luke0145555@hotmail.co.uk>
2019-11-06 15:43:39 +0000

    made settings option component and imported into settings page for styling

---
+++ client/src/components/settings-option/settings-option.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './settings-option.styles.scss';
+
+const SettingsOption = () => (
+    <div className="settings-option-container">
+        <span className="settings-option"> 
+            Text
+        </span>
+        <span>
+            Button
+        </span>
+    </div>
+)
+
+export default SettingsOption;
--- client/src/pages/settings/settings.component.jsx
+++ client/src/pages/settings/settings.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ToggleSwitch from '../../components/toggle-switch/toggle-switch.component';
+import SettingsOption from '../../components/settings-option/settings-option.component';
 
 import './settings.styles.scss';
 
@@ -8,22 +8,8 @@ const SettingsPage = () => (
     <div className="settings-container">
         <div className='settings-title'> Settings </div>
         <div className='list-container'>
-            <span className='list-item'>
-                 On / Off 
-                 <ToggleSwitch />
-                 </span>
-            <span className='list-item'>
-                 HD / LQ / Power Saver 
-                 {/* <ToggleSwitch /> */}
-                  </span>
-            <span className='list-item'> 
-            Cloud Storage 
-            {/* <ToggleSwitch /> */}
-            </span>
-            <span className='list-item'>
-                 Select Location
-                 {/* <ToggleSwitch /> */}
-                 </span>
+            <SettingsOption />
+            <SettingsOption />
         </div>
     </div>
 )
