c689e2410ec500d572f71ec3d7d2cb9600959293
luke01455 <luke0145555@hotmail.co.uk>
2019-11-06 20:59:39 +0000

    made account page display display some user info if there is a currentUser

--- client/src/pages/account/account.components.jsx
+++ client/src/pages/account/account.components.jsx
@@ -1,26 +1,46 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+
 import SettingsHeading from '../../components/settings-heading/settings-heading.component';
 import SettingsOption from '../../components/settings-option/settings-option.component';
 
 
 import './account.styles.scss';
 
-const AccountPage = () => (
-<div className="account-container">
-    <div className='header-section-container'>
-    <div className='account-title'> Account </div>
+const AccountPage = ({ currentUser }) => (
+    <div className="account-container">
+        <div className='header-section-container'>
+            <div className='account-title'> Account </div>
+            <div className='name-email-container'>
+                {currentUser ? (
+                    <div className='display-name'>{currentUser.displayName}</div>) :
+                    (<div></div>)}
+
+                {currentUser ? (
+                    <div className='display-email'>{currentUser.email}</div>) :
+                    (<div></div>)}
+
+            </div>
+        </div>
+
+        <SettingsHeading> Account Information </SettingsHeading>
+        <SettingsOption> Account </SettingsOption>
+        <SettingsOption> Personal Information </SettingsOption>
+        <SettingsOption> Change Password</SettingsOption>
+
+        <SettingsHeading> Premium Information </SettingsHeading>
+        <SettingsOption> Premium </SettingsOption>
+
+
     </div>
-    <SettingsHeading> Account Information </SettingsHeading>
-    <SettingsOption> Account </SettingsOption>
-    <SettingsOption> Personal Information </SettingsOption>
-    <SettingsOption> Change Password</SettingsOption>
-    
-    <SettingsHeading> Premium Information </SettingsHeading>
-    <SettingsOption> Premium </SettingsOption>
-
-
-</div>
 )
 
-export default AccountPage;
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser
+});
+
+export default connect(mapStateToProps)(AccountPage);
