47bab9fa3ed6024554fb34ab327cc84c1a91cf08
luke01455 <luke0145555@hotmail.co.uk>
2019-11-21 18:11:27 +0000

    added menu button to settings bar

--- client/src/components/settings-bar/settings-bar.component.jsx
+++ client/src/components/settings-bar/settings-bar.component.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import MenuButton from '../../components/menu-button/menu-button-component';
 import './settings-bar.styles.scss';
 
 const SettingsBar = ({children}) => (
+    <div className='menu-button-container'><MenuButton />
     <div className='bar-container'>
+        
+        
         <div className='bar-text'>
         {children}
         </div> 
+    </div>
     </div>
 )
 
