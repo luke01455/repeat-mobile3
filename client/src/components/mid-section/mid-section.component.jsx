41f08bbadfc93c674a31149411066aa3f45ea23e
luke01455 <luke0145555@hotmail.co.uk>
2019-11-24 19:20:04 +0000

    made method to get current recording time from backend and display it

--- client/src/components/mid-section/mid-section.component.jsx
+++ client/src/components/mid-section/mid-section.component.jsx
@@ -7,29 +7,48 @@ import RangeBar from '../../components/range-bar/range-bar.component';
 import './mid-section.styles.scss';
 
 
+class MidSection extends React.Component {
+  
+    // constructor(props) {
+    //     super(props);
+    //     
+    // }
 
-const MidSection = () => {
+    //const [minsTxt, setMinsTxt] = useState(10);
 
-    const [minsTxt, setMinsTxt] = useState(10);
+    componentDidMount() {
+
+    }
+
+    getCurrentTimer = () => {
+      if(window.MobileActions !== undefined) {
+        console.log('test')
+        return window.MobileActions.GetRecordTime();
+      }
+      else {
+        console.log('test')
+        return 0;
+      }
+    }
     
-    const saveRecording = () => {
-        window.MobileActions.Save(parseInt(minsTxt));
+    saveRecording = () => {
+        window.MobileActions.Save(parseInt(10));
       }
 
-    const retrieveSliderNum = (rangeNum) => {
-        setMinsTxt(rangeNum);
+    retrieveSliderNum = (rangeNum) => {
+        this.setMinsTxt(rangeNum);
     } 
-    
+  
 
-    return (
+    render() {
+      return (
         <div className="Middle-Section">
-
             <div className="center-container">
                 <div className="save-past-text"> Save the past...</div>
-                <div className="minutes-text"> {minsTxt} minutes </div>
-                <RangeBar callBackFromParent={retrieveSliderNum}/>
+                <div className="minutes-text"> {this.minsTxt} minutes </div>
+                <RangeBar callBackFromParent={this.retrieveSliderNum}/>
                 <div className='timer-organiser'>
-                <Timer initialTime={20000} id='timer-text'>
+                <Timer initialTime={this.getCurrentTimer()} id='timer-text'>
                 <Timer.Minutes /> minutes
                 <div className='spacer'></div>
                 <Timer.Seconds /> seconds
@@ -37,12 +56,47 @@ const MidSection = () => {
                 </div>
             </div>
             <div className="Savebtn-Container">
-                <button onClick={saveRecording} id="Savebtn">Save</button>
+                <button onClick={this.saveRecording} id="Savebtn">Save</button>
             </div>
         </div>
-    )
+      );
+    }
+  }
+// const MidSection = () => {
+
+//     const [minsTxt, setMinsTxt] = useState(10);
+    
+//     const saveRecording = () => {
+//         window.MobileActions.Save(parseInt(minsTxt));
+//       }
+
+//     const retrieveSliderNum = (rangeNum) => {
+//         setMinsTxt(rangeNum);
+//     } 
+    
+
+//     return (
+//         <div className="Middle-Section">
+
+//             <div className="center-container">
+//                 <div className="save-past-text"> Save the past...</div>
+//                 <div className="minutes-text"> {minsTxt} minutes </div>
+//                 <RangeBar callBackFromParent={retrieveSliderNum}/>
+//                 <div className='timer-organiser'>
+//                 <Timer initialTime={20000} id='timer-text'>
+//                 <Timer.Minutes /> minutes
+//                 <div className='spacer'></div>
+//                 <Timer.Seconds /> seconds
+//                 </Timer>
+//                 </div>
+//             </div>
+//             <div className="Savebtn-Container">
+//                 <button onClick={saveRecording} id="Savebtn">Save</button>
+//             </div>
+//         </div>
+//     )
 
     
-}
+// }
 
 export default MidSection;
